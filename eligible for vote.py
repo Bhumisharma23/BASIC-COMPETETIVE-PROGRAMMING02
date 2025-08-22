f079f056d6833af06e5ad2427f22324b0479dbda
Bhumisharma23 <bhumis1080161@gmail.com>
2025-08-22 20:38:43 +0530

    Add files via upload

---
+++ eligible for vote.py
@@ -0,0 +1,7 @@
+#wap to check whether a person is eligible for vote.
+
+age=int(input("enter age:"))
+if (age>=18):
+    print("person is eligible to vote")
+else:
+    print("person is not eligible to vote")
